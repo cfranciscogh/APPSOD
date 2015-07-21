b920d45290f6be7a0e08a3c5da5a7f6478348e58
Francisco <franz_568@hotmail.com>
2015-07-21 15:22:15 -0500

    rf
    gtraeeeeeeeeee

--- js/panel.js
+++ js/panel.js
@@ -35,10 +35,10 @@ function getProgramaciones(){
 				$("#contentProgramaciones").find("form, ul").fadeIn("fast");
 				var count = 0;
 				for (var i = 0; i<resultado.length;i++){
-					if ( resultado[i].IDEstado == 3 || resultado[i].IDEstado == 4  || resultado[i].IDEstado == 5 )
+					if (  resultado[i].IDEstado == 4  || resultado[i].IDEstado == 5 )
 		 				$("#listProgramacion").append('<li data-icon="check"><a>'+ resultado[i].NroOrdenCompra + ' - ' + resultado[i].NombreCliente +'</a></li> ');
 					else {
-						if (count==0)
+						if (count>=0)
 						$("#listProgramacion").append('<li><a data-ajax="false" href="detalle.html?IDPedido='+resultado[i].IDPedido+'&idChofer='+$.QueryString["idChofer"]+'">'+ resultado[i].NroOrdenCompra + ' - ' + resultado[i].NombreCliente +'</a></li> ');
 						else
 							$("#listProgramacion").append('<li data-icon="false"><a>'+ resultado[i].NroOrdenCompra + ' - ' + resultado[i].NombreCliente +'</a></li> ');
